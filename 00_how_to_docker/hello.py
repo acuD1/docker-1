47e78516319c8fd1d5428d358e1bf483351b0e19
acuD1 <arsciand@student.42.fr>
2019-01-25 11:15:18 +0100

    Part I Done

---
+++ 00_how_to_docker/hello.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
